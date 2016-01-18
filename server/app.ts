79658c1a33cae331723041e57b205e1f8b8391d1
Tim Padjen <tpadjen@gmail.com>
2016-01-18 10:43:22 -0700

    Statically serve node_modules and app requests from proper folders

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,9 @@ import * as path from 'path';
 var port: number = process.env.PORT || 3000;
 var app = express();
 
+app.use('/node_modules', express.static(path.resolve(__dirname, '..', 'node_modules')));
+app.use('/app', express.static(path.resolve(__dirname, '..', 'client', 'app')));
+
 app.get('/*', (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
 });
