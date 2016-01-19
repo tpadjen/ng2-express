e8dd93e2bd3538290582e7978d863eb30cb4a452
Tim Padjen <tpadjen@gmail.com>
2016-01-18 17:53:07 -0700

    Rename AppComponent tag to app

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>My App</h1>
+        <h1>App</h1>
     `
 })
 export class AppComponent {}
